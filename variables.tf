c413bd876783b1ad4f14cabdb7ae68af430a4427
Maalshekto <tmartin@live.fr>
2024-10-10 11:26:39 +0200

    debug

--- variables.tf
+++ variables.tf
@@ -3,14 +3,3 @@ variable "github_token" {
   type        = string
   sensitive   = true
 }
-variable "aws_access_key" {
-  description = "AWS Access Key"
-  type        = string
-  sensitive   = true
-}
-
-variable "aws_secret_key" {
-  description = "AWS Secret Key"
-  type        = string
-  sensitive   = true
-}
